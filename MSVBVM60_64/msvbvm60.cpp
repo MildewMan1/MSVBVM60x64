b1a2a1f13f292ff63bbd2e1ca452fbf3e04a87fa
Mark Haynes <miidewman1@aol.com>
2023-08-03 08:33:20 -0500

    final version of "vbaObjSetByAddress".

--- MSVBVM60_64/msvbvm60.cpp
+++ MSVBVM60_64/msvbvm60.cpp
@@ -1,86 +1,25 @@
 #include "pch.h"
 #include "unknwn.h"
 
-#include <string>
-
 #define ExtC extern "C" __declspec(dllexport) 
 #define VBA_CALL __stdcall
-#define VBA_FUNC(type) ExtC type VBA_CALL
-
-#define MY_DEBUG 0
-
-//IUnknown* __stdcall VBAObjSetByAddress(IUnknown* pUnknown, unsigned long* pRefCount)
-//{
-//
-//	if (pUnknown)
-//	{
-//		unsigned long refCount = pUnknown->AddRef();
-//		if (pRefCount != nullptr)
-//			*pRefCount = refCount;
-//	}
-//
-//	return pUnknown;
-//}
-
-#if MY_DEBUG
-
-unsigned long lastCount{ 0 };
-void* pLast{ nullptr };	
-#endif
-
-VBA_FUNC(unsigned long) vbaGetObjRefCount(IUnknown* pUnknown)
-{
-	
-	unsigned long retval{ 0 };
-
-	if (pUnknown != nullptr)
-	{
-		pUnknown->AddRef();
-		retval = pUnknown->Release();
-	}
-
-#if MY_DEBUG
-	static std::wstring msg{};
-	msg.reserve(100);
-
-	if (retval > 0 && (IUnknown*)pLast == pUnknown)
-	{
-		msg.clear();
-
-		msg = L"Last reference count = ";
-		msg += std::to_wstring(lastCount);
-		msg += L"\n";
-		msg += L"Current reference count = ";
-		msg += std::to_wstring(retval);
-		msg += L"\n";
-
-		OutputDebugString(msg.c_str());
-	}
-#endif
-	return retval;
-}
-
-inline IUnknown* __stdcall vbaObjSetByAddressTest(IUnknown* pUnknown, unsigned long* pRefCount = nullptr)
-{
-	if (pUnknown != nullptr)
-	{
-		unsigned long refCount = pUnknown->AddRef();
-
-		if (pRefCount != nullptr)
-			*pRefCount = refCount;
-	}
-	return pUnknown;
-}
+#define VBA_FUNC(_Type) ExtC _Type VBA_CALL
 
 VBA_FUNC(IUnknown*) vbaObjSetByAddress(IUnknown* pUnknown)
 {
+	/*
+	2023-Aug-03 
+	Per Microsoft's MSDN (https://learn.microsoft.com/en-us/windows/win32/api/unknwn/nf-unknwn-iunknown-addref):
+
+	"Call this method [AddRef] for every new copy of an interface pointer that you make. 
+	For example, if you return a copy of a pointer from a method, then you must 
+	call AddRef on that pointer."
 	
-#if MY_DEBUG
-	IUnknown* retval = vbaObjSetByAddressTest(pUnknown, &lastCount);
-	pLast = (void*)retval;
-	return retval;
-#else
-	return vbaObjSetByAddressTest(pUnknown);
-#endif
-	
+	*/
+
+	//check to make sure pUnknown isn't a nullptr before calling AddRef.
+	if (pUnknown != nullptr)	
+		pUnknown->AddRef();
+
+	return pUnknown;
 }
